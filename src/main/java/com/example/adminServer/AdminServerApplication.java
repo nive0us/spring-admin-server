b4e2d4cc3b6cbb433e0216614bef4ffa63a831c0
ni-laptop <wharddrive@gmail.com>
2020-05-13 13:44:59 +0800

    server UI
    Add dependency and Add Annotation

--- src/main/java/com/example/adminServer/AdminServerApplication.java
+++ src/main/java/com/example/adminServer/AdminServerApplication.java
@@ -1,9 +1,13 @@
 package com.example.adminServer;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableAdminServer
 @SpringBootApplication
+
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
